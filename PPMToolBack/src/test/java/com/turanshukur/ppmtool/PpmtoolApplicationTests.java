50ef8ff0de8eace0fdd8947acbe1f46f86ecc3ff
Turan <tshukurbeyli@gmail.com>
2020-02-24 15:47:05 +0900

    all

---
+++ PPMToolBack/src/test/java/com/turanshukur/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turanshukur.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
